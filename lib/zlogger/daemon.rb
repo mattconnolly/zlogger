950f42ab0d20c83e01008169aa5c7b1007318315
Gabriel Gizotti <gabriel.gizotti@mobilet.com.au>
2014-06-18 11:49:06 +1000

    options to write log to a file and to rotate file daily

--- lib/zlogger/daemon.rb
+++ lib/zlogger/daemon.rb
@@ -2,11 +2,16 @@ require 'rbczmq'
 
 module Zlogger
   class Daemon
-
     attr :options
+    attr_accessor :output_file
+    attr_accessor :log_date
 
     def initialize(options={})
       @options = options
+      if options[:output]
+        @log_date = Date.today
+        @output_file = File.new(output_filepath, 'a+')
+      end
     end
 
     def run
@@ -16,6 +21,8 @@ module Zlogger
 
       loop do
         begin
+          rotate_file if options[:rotate]
+
           message = socket.recv_message
           prefix = message.pop.to_s
           buffer = message.pop.to_s
@@ -23,6 +30,7 @@ module Zlogger
           buffer.split("\n").each do |line|
             log(prefix, line)
           end
+
         rescue Interrupt
           break
         rescue StandardError => e
@@ -44,7 +52,31 @@ module Zlogger
     end
 
     def output
-      options[:output] || $stdout
+      output_file || $stdout
+    end
+
+    def output_filepath
+      filename = options[:output]
+      file_extension = File.extname(filename)
+      filename = filename.gsub(file_extension, "") unless file_extension == ""
+      if options[:rotate]
+        filename = "#{ filename }_#{ log_date.to_s }.log"
+      else
+        filename = "#{ filename }.log"
+      end
+      Pathname.new('log').join(filename).to_s
+    end
+
+    def rotate_file
+      if output_file || log_date < Date.today
+        log_date = Date.today
+
+        # closes previews day file
+        output_file.close
+
+        # assigns file for the new day to output_file attribute
+        output_file = @output_file = File.new(output_filepath, 'a+')
+      end
     end
 
     def pub_socket
